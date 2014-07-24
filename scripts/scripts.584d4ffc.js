ccc2248c81dfd4141f358b88a4c3f1f4e2bd6c92
Will Lee-Wagner <william.lee.wagner@gmail.com>
2014-07-24 09:48:07 +0000

    Built prompt from commit b549da2 on branch master

--- scripts/scripts.584d4ffc.js
+++ scripts/scripts.584d4ffc.js
@@ -14,7 +14,8 @@ angular
     'ngSanitize',
     'duScroll',
     'LocalStorageModule',
-    'angularMoment'
+    'angularMoment',
+    'angularFileUpload'
   ])
   .config(['localStorageServiceProvider', function(localStorageServiceProvider){
     localStorageServiceProvider.setPrefix('ls');
@@ -94,17 +95,17 @@ angular.module('promptApp')
     // Set up a Prompt class 
     var scope = this,
         Prompt = function() {
-        return {
-            name: null,
-            body: null,
-            time: null
+            return {
+                name: null,
+                body: null,
+                time: null
+            };
         };
-    };
 
     // Tie a new prompt to the controller
     scope.prompt = new Prompt();
     
-    // Add a new prompt
+    // Add a new prompt by hand
     scope.addPrompt = function() {
         // Add prompt to db
         Prompts.push(scope.prompt);
@@ -341,14 +342,70 @@ angular.module('promptApp').factory('Resettable', function Resettable() {
  * # AllCtrl
  * Controller of the promptApp
  */
-angular.module('promptApp').controller('AllCtrl', ['$location', 'Prompts', function($location, Prompts) {
-    var scope = this;
+angular.module('promptApp').controller('AllCtrl', ['$scope', '$location', '$upload', 'Prompts', function($scope, $location, $upload, Prompts) {
+    var scope = this,
+        Prompt = function() {
+            return {
+                name: null,
+                body: null,
+                time: null
+            };
+        },
+        readerOnLoad =  (function (i, fileCount, file, reader, prompt) {
+            return function (e) {
+                // Populate the prompt object
+                prompt.name = file.name.replace(/\..+$/, '');
+                prompt.body = reader.result;
+                prompt.time = 0;
+                
+                // Add it to the Prompts array
+                Prompts.push(prompt);
+                
+                // If this is the last file, note that in the scope
+                if (i === fileCount-1) {
+                    scope.uploadStatus = scope.uStats.DONE;
+                    $scope.$apply();
+                }
+            };
+        });
+    
+    // Status Types
+    scope.uStats = {
+        NONE:       0,
+        STARTED:    1,
+        DONE:   2
+    };
+    
+    // current status
+    scope.uploadStatus = scope.uStats.NONE;
+    
+    // Upload a prompt file
+    scope.upload = function ($files) {
+        var fileCount = $files.length;
+        
+        if (fileCount) {
+            // Set status as started
+            scope.uploadStatus = scope.uStats.STARTED;
+        
+            // Loop through uploaded files
+            for (var i = 0; i < fileCount; i++) {
+                var file = $files[i],
+                    reader = new FileReader(),
+                    prompt = new Prompt();
+                
+                // set up reader callback 
+                reader.onload = readerOnLoad(i, fileCount, file, reader, prompt);
+                // read in the file
+                reader.readAsText(file);
+            }
+        }
+    };
     
     // Move to the prompt's play page
     scope.play = function (promptIndex) {
         $location.path('/play/' + promptIndex);
     };
-    // Move to the prompt's play page
+    // Move to the prompt's edit page
     scope.edit = function (promptIndex) {
         $location.path('/load/' + promptIndex);
     };

4bb824c27fa8b2055bb03e7ba43a25d6e6a55396
Will Lee-Wagner <william.lee.wagner@gmail.com>
2014-08-04 23:19:39 +0000

    Built prompt from commit e790b1c on branch master

--- scripts/scripts.c16e5d0b.js
+++ scripts/scripts.c16e5d0b.js
@@ -989,25 +989,78 @@ angular.module('promptApp')
     return {
       template: '<a class="btn btn-info" download="prompt.json" ng-enabled="export.isReady"><i class="fa fa-download"></i> Export</a>',
       restrict: 'E',
-      controller: function ($element, $q, Prompts) {
-        var scope = this;
+      controller: function ($element, $q, Prompts, phonegapStatus) {
+        var scope = this,
+            dataUri,
+            pg = phonegapStatus;
         
-        scope.isReady = false;
+        //======================================================================
+        // HELPERS =============================================================
+        //======================================================================
+        // Sets up click on phonegap
+        function phonegapSetup() {
+          var WebIntent = window.plugins.webintent,
+              extras = {};
+            
+          extras[WebIntent.EXTRA_SUBJECT] = 'prompt data export';
+          extras[WebIntent.EXTRA_TEXT]    = "My prompt data export is attached for importing.";
+          extras[WebIntent.EXTRA_STREAM]  = dataUri;
+          
+          // Phonegap doesn't do downloads. Use a intent to send the 
+          // data to email on button click
+          $element.on('click', function (e) {
+            WebIntent.startActivity(
+              {
+                action: WebIntent.ACTION_SEND,
+                extras: extras,
+              }, 
+              function() {
+                console.log('Success!');
+              }, 
+              function() {
+                alert('Failed to open URL via Android Intent');
+              }
+            );
+            
+            // Set the ready flag
+            scope.isReady = true;
+          });
+        }
         
-        // Ready a file for download
-        function readyFile() {
-          // Get the JSON for the prompts
-          var jsonPrompts = angular.toJson(Prompts.list);
-          
-          // Make it into a data uri for the element
-          $element.attr("href",'data:Application/octet-stream,'+encodeURIComponent(jsonPrompts));
-          
+        // Sets up click on browser
+        function browserClick() {
+          // Add the data to the button for download
+          $element.attr("href",dataUri);
           // Set the ready flag
           scope.isReady = true;
         }
         
+        // Ready the file for download
+        function readyFile() {
+          // Get the JSON for the prompts
+          var jsonPrompts = angular.toJson(Prompts.list);
+          
+          // generate the dataUri
+          dataUri = 'data:Application/octet-stream,'+encodeURIComponent(jsonPrompts);
+          
+          console.log('generated');
+        }
+        
+        $element.on('click', function () {
+          console.log('Clicked Export!');
+        });
+        
+        //======================================================================
+        // ON LOAD =============================================================
+        //======================================================================
+        // Init ready flag
+        scope.isReady = false;
+        
         // Get the file ready for download on load
         readyFile();
+        
+        // Set up click handler based on phonegap status
+        pg.then(phonegapSetup, browserClick);
       },
       controllerAs: 'export',
       replace: true
@@ -1071,3 +1124,41 @@ angular.module('promptApp')
       replace: true
     };
   });
+
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name promptApp.phonegapStatus
+ * @description
+ * # phonegapStatus
+ * Factory in the promptApp.
+ */
+angular.module('promptApp')
+  .factory('phonegapStatus', ['$q', function ($q) {
+    var deferred = $q.defer();
+    
+    // Return true if phonegap is running
+    function isPhoneGap() {
+     return !!window.PhoneGap;
+    }
+    
+    // resolve the deferred promise if phonegap exists, when ready
+    function onPhoneGapReady() {
+      deferred.resolve();
+    }
+    
+    // check if phonegap is installed
+    if (isPhoneGap()) {
+      // Add listener for deviceready
+      // And resolve the promise when it is ready
+      document.addEventListener("deviceready", yourCallbackFunction, false);
+    } else {
+      // Reject the promise if this isn't a phonegap instance
+      deferred.reject();
+    }
+    
+    
+    // Retun the promise
+    return deferred.promise;
+  }]);

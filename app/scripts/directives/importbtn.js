6302fa9bc19a595b5bd59aebbbf8fb2a48b591bc
Will Lee-Wagner <william.lee.wagner@gmail.com>
2014-08-11 00:41:06 +0000

    try/catch in imports

--- app/scripts/directives/importbtn.js
+++ app/scripts/directives/importbtn.js
@@ -36,9 +36,28 @@ angular.module('promptApp')
               // set up reader callback 
               // this will run after the file is parsed
               reader.onload = function (e) {
+                var newPrompts;
+                
                 // Replace the prompts with the file's info
-                Prompts.replace(angular.fromJson(decodeURIComponent(escape(reader.result))) || [])
-                // Move to the prompts page
+                try {
+                  // Try decoding the output from ANSII
+                  newPrompts = angular.fromJson(decodeURIComponent(escape(reader.result))) || [];
+                } catch (e) {
+                  try {
+                    // If that didn't work, it may be UTF-8
+                    // Try importing without decoding
+                    newPrompts = angular.fromJson(reader.result) || [];
+                  } catch (e) {
+                    // If that didn't work, there was a problem.
+                    // Write that to the console, and quit
+                    console.log('There was a problem with the import!');
+                    return;
+                  }
+                }
+                
+                // Replace the prompts list with the newprompts
+                Prompts.replace(newPrompts)
+                // Move to the prompts page when replace is finished
                 .then(function () {
                   $location.path('/');
                 });
